64152ea3d8a591c4d1a5f6bb7e165f5d59a93c3e
Peter Xia <aattyy@gmail.com>
2024-12-03 18:23:37 -0800

    update

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -7,7 +7,6 @@ while [ true ]; do
 		--plugin-dir "/home/.local/share/streamlink/plugins" \
 		--output /home/download/"{time:%Y%m%d%H%M%S}-{id}-{title}".mkv \
 		--hls-live-restart \
-		--retry-streams $RETRY_SECS \
 		"$STREAMLINK_OPTS" $STREAM_URL $QUALITY
-	sleep 10s
+	sleep ${RETRY_SECS}s
 done
